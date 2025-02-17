9e278f15c1ee9dcc41456f52fc27930cf65500fc
HazZav <kevlinlal@gmail.com>
2025-02-16 20:10:28 -0600

    Se implementó el método agregarAmigo().

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+“Cualquier idiota puede escribir un programa que una computadora 
+entiende, los verdaderos programadores pueden escribir código que 
+los humanos entienden.” – Martin Fowler
+*/
+
+//Atributos
+let arrayAmigos=[];
+
+//Métodos
+function limpiarCaja() {
+    document.getElementById('amigo').value = ''; ;
+}
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    
+    if (nombre !== ""){
+        arrayAmigos.push(nombre);
+        limpiarCaja();
+        console.log(arrayAmigos);
+    } else {
+        alert('Por favor, ingrese un nombre primero.');
+    }
+}
