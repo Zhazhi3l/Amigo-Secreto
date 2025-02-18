8cf575ba11bbc8f62fcd028bb8364a14cb907a5a
HazZav <kevlinlal@gmail.com>
2025-02-17 19:16:12 -0600

    Se agregó el método actualizar lista

--- app.js
+++ app.js
@@ -16,10 +16,27 @@ function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
     
     if (nombre !== ""){
+        for(let i = 0; i < arrayAmigos.length; i++){ //Revisar si ya existe el nombre en el array
+            if (nombre == arrayAmigos[i]){// Nota: Revisar si existe el equalsIgnoreCase
+                alert('¡Ya has escrito ese nombre!');
+                return;
+            }
+        }
         arrayAmigos.push(nombre);
         limpiarCaja();
+        actualizarLista();
         console.log(arrayAmigos);
     } else {
         alert('Por favor, ingrese un nombre primero.');
+    }
+}
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; //Limpia para evitar duplicados
+    for (let i = 0; i < arrayAmigos.length; i++){
+        let elementoLista = document.createElement('li');
+        elementoLista.textContent = arrayAmigos[i];
+        lista.appendChild(elementoLista);
     }
 }
